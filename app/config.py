8b1d3ecd5c9026400078bc6c3c3cb791616545e1
Ly Xuan Sang <sang.lx@teko.vn>
2019-09-20 01:45:43 +0700

    fix MONGO_URI and Dockerfile

--- app/config.py
+++ app/config.py
@@ -1,12 +1,25 @@
 import os
 import datetime
+import urllib.parse
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 
+def get_mongo_uri(username, password, host, port, database):
+    if username is None and password is None:
+        return f'mongodb://{host}:{port}/{database}'
+    password = urllib.parse.quote_plus(password)
+    return f'mongodb://{username}:' + password + f'@{host}:{port}/{database}?retryWrites=true'
+
+
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'something-very-secret'
-    MONGO_URI = os.environ.get('MONGO_URI') or 'mongodb://localhost:27017/cloudFDS'
+    MONGO_USERNAME = os.environ.get('MONGO_USERNAME')
+    MONGO_PASSWORD = os.environ.get('MONGO_PASSWORD')
+    MONGO_HOST = os.environ.get('MONGO_HOST') or 'localhost'
+    MONGO_PORT = os.environ.get('MONGO_PORT') or 27017
+    MONGO_DATABASE = os.environ.get('MONGO_DATABASE') or 'cloudFDS'
+    MONGO_URI = get_mongo_uri(MONGO_USERNAME, MONGO_PASSWORD, MONGO_HOST, MONGO_PORT, MONGO_DATABASE)
     JWT_SECRET_KEY = SECRET_KEY
     JWT_ACCESS_TOKEN_EXPIRES = datetime.timedelta(days=1)
 
