3d64c646a13231dc8025ece505c89fe89ba0db7e
Ly Xuan Sang <lyxuansang1991@gmail.com>
2019-09-30 00:45:35 +0700

    fix bug msgId too long

--- app/email.py
+++ app/email.py
@@ -10,6 +10,7 @@ def send_async_email(app, msg):
 
 def send_email(subject, sender, recipients, html_body):
     msg = Message(subject, sender=sender, recipients=recipients)
+    msg.msgId = msg.msgId.split('@')[0] + '@short_string'
     msg.html = html_body
     from manage import app
     Thread(target=send_async_email, args=(app, msg)).start()
